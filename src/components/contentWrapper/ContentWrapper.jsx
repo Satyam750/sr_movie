b31d21d4420c5ef7e1dbcdbfe7e78e606d14cb72
Satyam750 <satyamthakur05@gmail.com>
2023-10-27 23:39:56 +0530

    first commit

---
+++ src/components/contentWrapper/ContentWrapper.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./contactWrappers.scss";
+
+const ContentWrapper = ({ children }) => {
+    return <div className="contentWrapper">{children}</div>;
+};
+
+export default ContentWrapper;
+
+
+// CSS
---
+++ src/pages/404/PageNotFound.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./pageNotFounds.scss"
+import ContentWrapper from '../../components/contentWrapper/ContentWrapper'
+const PageNotFound = () => {
+  return (
+    <div>
+      <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    </div>
+  )
+}
+
+export default PageNotFound
